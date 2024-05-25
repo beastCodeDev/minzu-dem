27192dff80437f60baed4e22eb63e5caa52aded4
Kr31nZss <Kr31nZss210907@mail.ru>
2024-05-25 19:04:01 +0600

    feat:added prettier and eslint

---
+++ .eslintrc.js
@@ -0,0 +1,34 @@
+const { EndOfLineState } = require('typescript');
+
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+  },
+  extends: [
+    'eslint:recommended',
+    'plugin:react/recommended',
+    'plugin:prettier/recommended',
+    'airbnb',
+  ],
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
+    },
+    ecmaVersion: 12,
+    sourceType: 'module',
+  },
+  plugins: ['react'],
+  rules: {
+    'no-var': 'error',
+    'prefer-const': 'warn',
+    'react/prop-types': 'off',
+    'react/react-in-jsx-scope': 'off',
+    'prettier/prettier': [
+      'warn',
+      {
+        EndOfLineState: 'auto',
+      },
+    ],
+  },
+};
