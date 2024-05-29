6a6ace49848e2a12cd3cea85dee0c3160fab4491
Diyar_plit99 <144979597+DIARPRAGRAMIST@users.noreply.github.com>
2024-05-29 10:47:34 +0600

    layout-of-contact-me

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,9 +1,10 @@
+import ContactMePages from '../pages/ContactMePages/ContactMePages';
 import './styles/App.css';
 
 function App() {
   return (
     <>
-      <h1 className="text-3xl font-bold underline">Hello world!</h1>
+    <ContactMePages />
     </>
   );
 }
---
+++ src/pages/ContactMePages/ContactMePages.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import ContactMeWidget from '../../widgets/ContactMeWidgets/ContactME.Widget';
+
+const ContactMePages = () => {
+    return (
+        <div>
+            <ContactMeWidget />
+        </div>
+    );
+}
+
+export default ContactMePages;
