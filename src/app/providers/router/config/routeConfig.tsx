80a923af065e288e26e10282e561ab69159f7c73
Mairambek <kmr101207@gmail.com>
2024-06-08 05:07:13 +0600

    Fixed the adaptive

--- src/app/providers/router/config/routeConfig.tsx
+++ src/app/providers/router/config/routeConfig.tsx
@@ -2,17 +2,20 @@ import { RouteProps } from 'react-router-dom';
 
 import { AdminBio } from '~pages/admin/bio';
 import { PaintingsAdmin } from '~pages/admin/paintingsAdmin';
+import { BioPages } from '~pages/site/Bio';
 import { Home } from '~pages/site/home';
 
 enum AppRoutes {
   Home = 'home',
   AdminBio = 'adminBio',
   AdminPaintings = 'adminPaintings',
+  Bio = 'bio',
 }
 export const routePaths: Record<AppRoutes, string> = {
   [AppRoutes.Home]: '/',
   [AppRoutes.AdminBio]: '/admin/bio',
   [AppRoutes.AdminPaintings]: '/admin/paintings',
+  [AppRoutes.Bio]: '/bio',
 };
 
 export const routerConfig: Record<AppRoutes, RouteProps> = {
@@ -29,5 +32,10 @@ export const routerConfig: Record<AppRoutes, RouteProps> = {
   [AppRoutes.AdminPaintings]: {
     path: routePaths.adminPaintings,
     element: <PaintingsAdmin />,
+  },
+
+  [AppRoutes.Bio]: {
+    path: routePaths.bio,
+    element: <BioPages />,
   },
 };
---
+++ src/pages/site/Bio/index.ts
@@ -0,0 +1,3 @@
+import BioPages from "./ui/BioPages";
+
+export { BioPages }
